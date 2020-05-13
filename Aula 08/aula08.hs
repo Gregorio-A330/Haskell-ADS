df547288d7b9337206653b3af604867044b4adf1
Gabriel Gregorio <36648592+Gregorio-A330@users.noreply.github.com>
2020-05-12 22:02:54 -0300

    Aulas 05, 06, 07 e 08
    Incluindo o restante do material feito em aula

---
+++ Aula 08/aula08.hs
@@ -0,0 +1,17 @@
+module AulaOito where
+
+    -- Def: Uma mônada em uma categoria C
+    -- Eh a tripla (m , return, join), Onde
+    --  - m EH UM FUNTOR
+    --  return :: a -> m a TRANSF. NATURAL
+    --  join :: m (m a) -> m a TRANSF. NATURAL
+
+    -- Satisfazendo:
+        -- a) RETURN EH O ELEMENTO NEUTRO
+        -- b) ASSOCIATIVIDADE: fmap join . join = join . join
+    
+    -- UMA MONADA EH APENAS UM MONOIDE NA CATEGORIA DOS (ENDO)FUNTORES;
+
+    class Monoid m where
+        mempty :: m
+        mappend :: m -> m -> m
